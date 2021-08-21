20237f15fa04ebdf01698e6287a00583eddcb2be
mumboFromAvnotaklu <sukhmander111101@gmail.com>
2021-08-21 09:57:24 +0530

    deleted some temp files

---
+++ README.md
@@ -0,0 +1,7 @@
+# djangoChatApp
+django-channel based chat app
+## Description - Asynchoronous django app built with channels
+## Requirements
+* python3 3.8
+* redis-server v=6.2.5 (
+* 
--- testing/fixtures/cal_msgs.py
+++
@@ -1,8 +0,0 @@
-import json
-with open("fiftyThousandMsgs.json","r") as f:
-    json_data = json.loads(f.read());
-    pk = 1
-    for model in json_data:
-        if model['model'] == 'chat.message':
-            pk += 1
-    print(pk)
